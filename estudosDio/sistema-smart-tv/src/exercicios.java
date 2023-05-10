8427a4225a1f19ed32cd1dd25ec95acb3550e1df
AlmirMenezesJr <jrjmswii@gmail.com>
2023-05-10 08:22:55 -0300

    ver atualizaçao do projeto

---
+++ estudosDio/sistema-smart-tv/src/exercicios.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class exercicios {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("toto");
+        Scanner teclado = new Scanner(System.in);
+
+       int num = teclado.nextInt();
+
+       teclado.close();
+
+       System.out.println(num);
+
+    }
+}
---
+++ java+BD/Java_BD_Novo/src/java_bd_novo/Java_BD_Novo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_bd_novo;
+
+import conexao.Conexao;
+
+/**
+ *
+ * @author jr_jm
+ */
+public class Java_BD_Novo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Conexao c = new Conexao();
+        
+        
+    }
+    
+}
